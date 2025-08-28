051a7d137271a6cd4d8d45775fb8540a363c3076
danilbolomatov-ux <danil.bolomatov@gmail.com>
2025-08-28 18:13:24 +0300

    fix pwa

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 
-const CACHE = 'schedule-cache-v1';
+const CACHE = 'schedule-cache-v2';
 const ASSETS = [
   './',
   './index.html',
@@ -11,7 +11,9 @@ self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k!==CACHE).map(k => caches.delete(k)))));
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+  );
 });
 self.addEventListener('fetch', e => {
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
